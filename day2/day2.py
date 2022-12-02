52c42cfd7bc523d829a2330cd58b72f62b56d484
Jeremy Casey <jeremy8883@gmail.com>
2022-12-02 17:16:27 +1100

    Day 2, Pt 2

--- day2/day2.py
+++ day2/day2.py
@@ -4,18 +4,7 @@ opponent_play_map = {
     "C": 3, # Scissors
 }
 
-# These values also happen to equal the scores.
-# The winning play is also always one score higher.
-you_play_map = {
-    "X": 1, # Rock
-    "Y": 2, # Paper
-    "Z": 3, # Scissors
-}
-
-def get_score(opponent_play, you_play):
-    opp = opponent_play_map[opponent_play]
-    you = you_play_map[you_play]
-
+def get_score(opp, you):
     if you == opp :
         return you + 3
     elif (you % 3 + 1) == opp:
@@ -23,13 +12,44 @@ def get_score(opponent_play, you_play):
     else:
         return you + 6
 
+def get_score_by_target_play(opponent_play, you_play):
+    # These values also happen to equal the scores.
+    # The winning play is also always one score higher.
+    you_play_map = {
+        "X": 1,  # Rock
+        "Y": 2,  # Paper
+        "Z": 3,  # Scissors
+    }
+
+    opp = opponent_play_map[opponent_play]
+    you = you_play_map[you_play]
+
+    return get_score(opp, you)
 
 def part_1(data):
     rounds = data.split("\n")
-    scores = list(map(lambda r: get_score(r[0], r.split(" ")[1]), rounds))
+    scores = list(map(lambda r: get_score_by_target_play(r[0], r.split(" ")[1]), rounds))
+    return sum(scores)
+
+def get_score_by_desired_outcome(opponent_play, desired_outcome):
+    opp = opponent_play_map[opponent_play]
+
+    if desired_outcome == "X": # Lose
+        you = (opp - 2) % 3 + 1
+    elif desired_outcome == "Y": # Draw
+        you = opp
+    else: # Win
+        you = opp % 3 + 1
+
+    return get_score(opp, you)
+
+def part_2(data):
+    rounds = data.split("\n")
+    scores = list(map(lambda r: get_score_by_desired_outcome(r[0], r.split(" ")[1]), rounds))
     return sum(scores)
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as file:
         data = file.read()
     print(part_1(data))
+    print(part_2(data))
--- day2/day2.test.py
+++ day2/day2.test.py
@@ -1,5 +1,5 @@
 import unittest
-from day2 import part_1
+from day2 import part_1, part_2
 
 class TestDay(unittest.TestCase):
 
@@ -9,6 +9,11 @@ class TestDay(unittest.TestCase):
         self.assertEqual(part_1("C Z"), 6)
         self.assertEqual(part_1("A Y\nB X\nC Z"), 15)
         self.assertEqual(part_1("C Y"), 0 + 2)
+    def test_part_2(self):
+        self.assertEqual(part_2("A Y"), 4)
+        self.assertEqual(part_2("B X"), 1)
+        self.assertEqual(part_2("C Z"), 7)
+        self.assertEqual(part_2("A Y\nB X\nC Z"), 12)
 
 if __name__ == '__main__':
     unittest.main()
