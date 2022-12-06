931125438e09e7176036b2f290ef2ac143a6b25e
Jeremy Casey <jeremy8883@gmail.com>
2022-12-06 23:15:12 +1100

    Create template

---
+++ dayX/dayX.py
@@ -0,0 +1,16 @@
+from ramda import pipe
+
+def parse_data(data):
+    return data.split("\n")
+
+def part_1(data):
+    return "TODO"
+
+def part_2(data):
+    return "TODO"
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as file:
+        data = file.read()
+    print(part_1(data))
+    print(part_2(data))
---
+++ dayX/dayX.test.py
@@ -0,0 +1,13 @@
+import unittest
+from dayX import part_1
+
+class TestDay(unittest.TestCase):
+
+    def test_part_1(self):
+        self.assertEqual(
+            part_1("TODO"),
+            "TODO"
+        )
+
+if __name__ == '__main__':
+    unittest.main()
